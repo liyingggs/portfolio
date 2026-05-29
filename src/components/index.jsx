58f07429feba5c2e413891d80f8b2af613976c11
liyingggs <liyingggs@gmail.com>
2026-05-29 22:53:42 +0800

    updated index 29may

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -9,7 +9,7 @@ const Index = () => {
         <h1>Hello! I'm <span>Ong Li Ying</span></h1>
         <p className="hero-subtitle"> Aspiring Full Stack Developer | Cloud Enthusiast | Tech Student</p>
         <p className="hero-description">
-          Diploma in Enterprise Cloud Computing & Management with expertise in Software Development, and Cloud Technologies
+          Diploma in Information Technology with expertise in Software Development, and Cloud Technologies
         </p>
         <div className="hero-buttons">
           <a href="#projects" className="btn btn-primary">View My Work</a>
